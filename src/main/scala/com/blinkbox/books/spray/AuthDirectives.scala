dd59c4de786e4fb58a6f357490c7dfcc4cc4b023
Egidijus Jankauskas <egidijus@blinkbox.com>
2014-06-27 12:11:53 +0100

    AuthToken directive

--- src/main/scala/com/blinkbox/books/spray/AuthDirectives.scala
+++ src/main/scala/com/blinkbox/books/spray/AuthDirectives.scala
@@ -4,17 +4,25 @@ import scala.language.implicitConversions
 import spray.http.HttpHeaders.Authorization
 import spray.http.OAuth2BearerToken
 import spray.routing._
-import spray.routing.AuthenticationFailedRejection.CredentialsRejected
+import spray.routing.AuthenticationFailedRejection.{CredentialsMissing, CredentialsRejected}
 import spray.routing.Directives.{authenticate, optionalHeaderValueByType, provide, reject}
 import spray.routing.directives.AuthMagnet
 
-import com.blinkbox.books.spray.ZuulTokenAuthenticator.credentialsInvalidHeaders
+import com.blinkbox.books.spray.ZuulTokenAuthenticator.{credentialsMissingHeaders, credentialsInvalidHeaders}
 
 object AuthDirectives {
 
   def optionalAuthenticate[T](magnet: AuthMagnet[T]): Directive1[Option[T]] = optionalHeaderValueByType[Authorization]().flatMap {
     case Some(_) => authenticate(magnet).flatMap(u => provide(Some(u)))
     case None => provide(None)
+  }
+
+  val authToken: Directive1[String] = optionalHeaderValueByType[Authorization]().flatMap {
+    case None         => reject(AuthenticationFailedRejection(CredentialsMissing, credentialsMissingHeaders))
+    case Some(header) => extractAuthToken(header) match {
+      case Right(token)    => provide(token)
+      case Left(rejection) => reject(rejection)
+    }
   }
 
   val optionalAuthToken: Directive1[Option[String]] = optionalHeaderValueByType[Authorization]().flatMap {
---
+++ src/test/scala/com/blinkbox/books/spray/AuthTokenDirectiveTests.scala
@@ -0,0 +1,39 @@
+package com.blinkbox.books.spray
+
+import com.blinkbox.books.spray.AuthDirectives.authToken
+import com.blinkbox.books.spray.ZuulTokenAuthenticator.{credentialsInvalidHeaders, credentialsMissingHeaders}
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import spray.http.HttpHeaders.Authorization
+import spray.http.{GenericHttpCredentials, OAuth2BearerToken}
+import spray.routing.AuthenticationFailedRejection
+import spray.routing.AuthenticationFailedRejection.{CredentialsMissing, CredentialsRejected}
+import spray.routing.Directives.complete
+import spray.testkit.ScalatestRouteTest
+
+@RunWith(classOf[JUnitRunner])
+class AuthTokenDirectiveTests extends FunSuite with ScalatestRouteTest {
+
+  val route = authToken { x =>
+    complete(x)
+  }
+
+  test("Extracts token from supported Authorization header") {
+    Get("/") ~> Authorization(OAuth2BearerToken("token")) ~> route ~> check {
+      assert(responseAs[String] === "token")
+    }
+  }
+
+  test("Rejects when Authorization header is missing") {
+    Get("/") ~> route ~> check {
+      assert(rejection === AuthenticationFailedRejection(CredentialsMissing, credentialsMissingHeaders))
+    }
+  }
+
+  test("Rejects unsupported Authorization header") {
+    Get("/") ~> Authorization(GenericHttpCredentials("god", "Argy")) ~> route ~> check {
+      assert(rejection === AuthenticationFailedRejection(CredentialsRejected, credentialsInvalidHeaders))
+    }
+  }
+}
