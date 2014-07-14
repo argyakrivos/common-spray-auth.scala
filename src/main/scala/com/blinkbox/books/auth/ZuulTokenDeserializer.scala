f9986c3ca4b0a9bd6ff66048036df39a9be008aa
Greg Beech <greg@blinkbox.com>
2014-07-14 11:18:01 +0100

    Added claims & roles to the User object
    ### Breaking changes
    
    - The `User` constructor now takes an additional `claims` parameter
    containing the additional claims about the user.
    
    ### New features
    
    - You can now access all the claims about a user.
    - The roles claim is parsed into a set of `UserRole` values on demand.
    
    ### Bug fixes
    
    - `ZuulTokenAuthenticator` doesn’t check elevation when the desired
    elevation is `Unelevated`.

--- src/main/scala/com/blinkbox/books/auth/ZuulTokenDeserializer.scala
+++ src/main/scala/com/blinkbox/books/auth/ZuulTokenDeserializer.scala
@@ -1,33 +1,12 @@
 package com.blinkbox.books.auth
 
-import com.blinkbox.security.jwt.{InvalidClaimException, InvalidTokenException, TokenDecoder}
+import com.blinkbox.security.jwt.{InvalidTokenException, TokenDecoder}
 import scala.concurrent.{ExecutionContext, Future}
 
 class ZuulTokenDeserializer(decoder: TokenDecoder)(implicit val executionContext: ExecutionContext) extends TokenDeserializer {
-  private val UserUrn = """urn:blinkbox:zuul:user:(\d+)""".r
-  private val ClientUrn = """urn:blinkbox:zuul:client:(\d+)""".r
-
   override def apply(token: String): Future[User] = Future(decoder.decode(token)) flatMap {
-    case claims: java.util.Map[_, _] => user(claims)
+    case claims: java.util.Map[_, _] => Future(User(claims.asInstanceOf[java.util.Map[String, AnyRef]]))
     case _ => Future.failed(new InvalidTokenException("The token does not contain valid claims"))
-  }
-
-  private def user(claims: java.util.Map[_, _]): Future[User] = Future { User(userId(claims), clientId(claims)) }
-
-  private def userId(claims: java.util.Map[_, _]): Int = claims.get("sub") match {
-    case UserUrn(id) => identifier(id)
-    case _ => throw new InvalidClaimException("The token does not contain a valid user id claim.")
-  }
-
-  private def clientId(claims: java.util.Map[_, _]): Option[Int] = Option(claims.get("bb/cid")) map {
-    case ClientUrn(id) => identifier(id)
-    case _ => throw new InvalidClaimException("The token contains an invalid client id claim.")
-  }
-
-  private def identifier(id: String) = try {
-    id.toInt
-  } catch {
-    case e: NumberFormatException => throw new InvalidClaimException("The identifier is outside the expected range.")
   }
 }
 
