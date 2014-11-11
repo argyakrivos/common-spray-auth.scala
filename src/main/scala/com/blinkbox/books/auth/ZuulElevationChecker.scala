5a36ed26a39a7dc8f001f5b2f2b531686ffb0fae
Greg Beech <greg@blinkbox.com>
2014-11-11 16:24:16 +0000

    Elevated sessions are now extended
    ### Bug Fixes
    
    * When checking elevation, sessions are now extended by using `POST`
    rather than simply checked using `GET`. This means that expiration is
    sliding (as it should be) rather than absolute.

--- src/main/scala/com/blinkbox/books/auth/ZuulElevationChecker.scala
+++ src/main/scala/com/blinkbox/books/auth/ZuulElevationChecker.scala
@@ -6,13 +6,16 @@ import com.blinkbox.books.auth.Elevation._
 import org.json4s.DefaultFormats
 import org.json4s.ext.EnumNameSerializer
 import org.json4s.jackson.JsonMethods._
+import spray.http.HttpHeaders.{Authorization, Accept, `WWW-Authenticate`}
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import spray.client.pipelining._
 import spray.http._
 import spray.http.HttpCharsets._
 import spray.http.MediaTypes._
+import spray.http.StatusCodes._
 import spray.httpx.Json4sJacksonSupport
+import spray.httpx.UnsuccessfulResponseException
 import spray.httpx.unmarshalling.Unmarshaller
 
 private case class ZuulSession(tokenElevation: Elevation)
@@ -22,16 +25,20 @@ class ZuulElevationChecker(sessionUri: String)(implicit val actorRefFactory: Act
   implicit val timeout = Timeout(2.seconds)
   implicit val json4sJacksonFormats = DefaultFormats + new EnumNameSerializer(Elevation)
 
-  implicit def unmarshaller[T: Manifest] = Unmarshaller[T](`application/json`) {
+  implicit def unmarshaller[T: Manifest]: Unmarshaller[T] = Unmarshaller[T](`application/json`) {
     case entity: HttpEntity.NonEmpty => parse(entity.asString(defaultCharset = `UTF-8`)).camelizeKeys.extract[T]
   }
 
   private def pipeline(token: String): HttpRequest => Future[ZuulSession] = (
-    addHeader("Accept", "application/json")
-      ~> addHeader("Authorization", s"Bearer $token")
-      ~> sendReceive
-      ~> unmarshal[ZuulSession]
-    )
+    addHeader(Accept(`application/json`))
+    ~> addHeader(Authorization(OAuth2BearerToken(token)))
+    ~> sendReceive
+    ~> unmarshal[ZuulSession]
+  )
 
-  def apply(user: User): Future[Elevation] = pipeline(user.accessToken)(Get(sessionUri)).map(_.tokenElevation)
+  def apply(user: User): Future[Elevation] = pipeline(user.accessToken)(Post(sessionUri)) map {
+    _.tokenElevation
+  } recover {
+    case e: UnsuccessfulResponseException if e.response.status == Unauthorized => Unelevated
+  }
 }
